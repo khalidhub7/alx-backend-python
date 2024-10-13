124845e47d4b0babed1c9314f92d2d7b716d6125
khalidhub7 <louguikhalid7@gmail.com>
2024-10-13 00:16:42 -0700

    typevar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
-"""a type-annotated function"""
-import typing
-
-
-class Mytyping:
-    """help class"""
-    T = typing.TypeVar('T')
+""" basic annotations """
+from typing import Union, Any, TypeVar, Mapping
+T = TypeVar('T')
 
 
 def safely_get_value(
-    dct: typing.Mapping,
-    key: typing.Any,
-    default: typing.Union[Mytyping.T, None] = None
-) -> typing.Union[typing.Any, Mytyping.T]:
-    """a type-annotated function"""
+    dct: Mapping[Any, T], key: Any,
+        default: Union[T, None] = None
+) -> Union[T, None]:
+    """ typevar """
     if key in dct:
         return dct[key]
     else:
