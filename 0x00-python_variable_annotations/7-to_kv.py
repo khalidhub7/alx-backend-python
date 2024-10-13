d928b6503987e2e70bde07232ae379986d1bbdea
khalidhub7 <louguikhalid7@gmail.com>
2024-10-12 23:20:10 -0700

    square

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""a type-annotated function"""
+""" basic annotations """
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """a type-annotated function"""
-    return (k, v**2)
+    """ square """
+    return (k, float(v ** 2))
