4403e990b18a1a25f6639ddadf6ab44e4a3bf7df
khalidhub7 <louguikhalid7@gmail.com>
2024-10-12 22:56:11 -0700

    sum(list of floats)

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""a type-annotated function"""
+""" basic annotations """
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """a type-annotated function"""
-    sum: float = 0.0
-    for n in input_list:
-        sum += n
+    """ sum """
+    sum: float = 0.00
+    for i in input_list:
+        sum += i
     return sum
