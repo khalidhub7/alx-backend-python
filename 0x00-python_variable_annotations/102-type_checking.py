4de71e5a358b7b979be7f714af4e76e6ff192904
khalidhub7 <louguikhalid7@gmail.com>
2024-10-13 00:34:20 -0700

    type checking : mypy

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
-"""a type-annotated function"""
-from typing import List, Tuple
+""" basic annotations """
+from typing import Union, Any, TypeVar, Mapping, Tuple, List
+T = TypeVar('T')
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """a type-annotated function"""
-    zoomed_in: List = [
+def zoom_array(lst: List[int], factor: int = 2
+               ) -> List[int]:
+    zoomed_in: List[int] = [
         item for item in lst
-        for i in range(factor)
-    ]
+        for i in range(factor)]
     return zoomed_in
 
 
-array = tuple([12, 72, 91])
-
+array = [12, 72, 91]
 zoom_2x = zoom_array(array)
-
-zoom_3x = zoom_array(array, int(3.0))
+zoom_3x = zoom_array(array, 3)
