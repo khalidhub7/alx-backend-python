eab3732124b4b1ab0dbfa7c7eb59899313993499
khalidhub7 <louguikhalid7@gmail.com>
2024-10-14 00:59:01 -0700

    independent task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ basics of async """
 import asyncio
-wait_ran = __import__('prev').wait_random
+wait_ran = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay):
