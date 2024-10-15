a9bc9834d5faccbcc54fca62eef4aafa4cd868a2
khalidhub7 <louguikhalid7@gmail.com>
2024-10-15 16:34:37 -0700

    async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,9 @@ from typing import Generator
 
 async def async_generator(
 ) -> Generator[float, None, None]:
-    """ yield """
+    """ arg1: Yield (float)
+        arg2: Send (None)
+        arg3: Return (None) """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
