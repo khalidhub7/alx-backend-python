2fa9887a7c6d2529563ececd57dc5f0a4afa16d0
khalidhub7 <louguikhalid7@gmail.com>
2024-11-03 16:25:06 -0800

    parameterized handle raises

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -24,7 +24,7 @@ class TestAccessNestedMap(TestCase):
 
     @parameterized.expand([
         ({}, ("a",), "a"),
-        ({"a": 1}, ("a", "b"), ("b")),
+        ({"a": 1}, ("a", "b"), "b"),
     ])
     def test_access_nested_map_exception(
             self, nested_map: Dict, path: Tuple[str],
