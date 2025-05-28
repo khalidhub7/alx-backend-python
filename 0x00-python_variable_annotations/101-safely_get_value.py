670cc17e19eed657ebdfd7ca7bbe5b2345548b98
khalidhub7 <louguikhalid7@gmail.com>
2025-05-28 22:35:56 +0100

    generic typing

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 """ basic annotations """
-from typing import Union, Any, TypeVar, Mapping
+from typing import Any, Union, TypeVar, Mapping
 T = TypeVar('T')
 
 
-def safely_get_value(
-    dct: Mapping, key: Any,
-    default: Union[T, None] = None
-) -> Union[Any, T]:
-    """ typevar """
+def safely_get_value(dct: Mapping, key: Any, default:
+                     Union[T, None] = None
+                     ) -> Union[Any, T]:
+    """ get dict value safely """
     if key in dct:
         return dct[key]
     else:
