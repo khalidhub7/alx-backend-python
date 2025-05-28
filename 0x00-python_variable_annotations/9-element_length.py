aafa95c25ad28609f549535a367ab4322c11870e
khalidhub7 <louguikhalid7@gmail.com>
2025-05-28 21:22:41 +0100

    If it walks like a duck and quacks like a duck, it’s a duck

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ basic annotations """
-from typing import Iterable, List, Tuple, Sequence
+from typing import Iterable, Sequence, Tuple, List
 
 
 def element_length(lst: Iterable[Sequence]
                    ) -> List[Tuple[Sequence, int]]:
-    """ function annotations """
+    """ pair each element with its len """
     return [(i, len(i)) for i in lst]
