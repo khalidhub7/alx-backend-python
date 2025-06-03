2431ffb4590e834fb990540c489c4f688e72c4f5
khalidhub7 <louguikhalid7@gmail.com>
2025-06-03 18:04:18 +0100

    scheduled a Task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,7 +6,7 @@ wait_random = __import__(
     '0-basic_async_syntax').wait_random
 
 
-async def task_wait_random(max_delay: int
+def task_wait_random(max_delay: int
                            ) -> asyncio.Task:
     """ return a scheduled Task for wait_random """
 
