c9dbc52894f5072dad0db4cab5b789b9de8532c7
khalidhub7 <louguikhalid7@gmail.com>
2025-06-03 20:16:17 +0100

    updated version of 3-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 """ basics of async """
+
 import asyncio
-import random
 from typing import List
+task_wait_random = __import__(
+    '3-tasks').task_wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    """ random """
-    gen = random.uniform(0, max_delay)
-    await asyncio.sleep(gen)
-    return gen
+async def task_wait_n(n: int, max_delay: int
+                      ) -> List[float]:
+    """ run wait_random n times concurrently """
 
+    tasks = [
+        task_wait_random(max_delay)
+        for _ in range(n)
+    ]
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ multiplee coroutines """
-    tasks = []
-    for i in range(n):
-        tasks.append(wait_random(max_delay))
-    all = asyncio.as_completed(tasks)  # sort tasks
-    finished = []
-    for j in all:
-        result = await j
-        finished.append(result)
+    finished = [
+        await task
+        for task in asyncio.as_completed(tasks)
+    ]
+
     return finished
