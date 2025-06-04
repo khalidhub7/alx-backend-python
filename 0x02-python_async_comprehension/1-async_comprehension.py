2daa868b768ab7f6886143a544210ffaffead446
khalidhub7 <louguikhalid7@gmail.com>
2025-06-04 20:53:17 +0100

    collect nums using async_generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-""" async comprehension """
+""" python async comprehension """
+
 from typing import List
-async_gen = __import__(
+async_generator = __import__(
     '0-async_generator').async_generator
 
 
-async def async_comprehension(
-) -> List[float]:
-    """ async for """
-    return [i async for i in async_gen()]
+async def async_comprehension() -> List[float]:
+    """ async Comprehensions """
+
+    return [
+        i async for i in async_generator()
+    ]
