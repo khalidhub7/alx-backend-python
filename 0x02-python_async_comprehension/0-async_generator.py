673b62d68615a4f4ff6a6dba5ce83e8ffe976892
khalidhub7 <louguikhalid7@gmail.com>
2025-06-04 05:39:00 +0100

    async generator ?&

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,11 +3,10 @@
 
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator(
-) -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """ async Generator """
 
     for _ in range(10):
